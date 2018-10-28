24dbfb581c95105654ef8b07b966c7b1613011ca
Juan Cabello <jcabelloc@gmail.com>
2018-10-27 23:26:33 -0500

    Design Registro de Gasto UI Operation

--- src/app/operaciones/administrativas/registro-gasto/components/create-registro-gasto/create-registro-gasto.component.ts
+++ src/app/operaciones/administrativas/registro-gasto/components/create-registro-gasto/create-registro-gasto.component.ts
@@ -1,11 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Option {
+  value: string;
+  viewValue: string;
+}
+
+export interface Proveedor {
+  tipoIdentificacion: string;
+  nroIdentificacion: string;
+  nombre: string;
+  cuenta: string;
+}
+
+export interface Gasto {
+  rubro: string,
+  nota: string,
+}
+
+
 @Component({
   selector: 'app-create-registro-gasto',
   templateUrl: './create-registro-gasto.component.html',
   styleUrls: ['./create-registro-gasto.component.css']
 })
 export class CreateRegistroGastoComponent implements OnInit {
+  
+  proveedor: Proveedor = {
+    tipoIdentificacion: 'RUC',
+    nroIdentificacion: '2003728693',
+    nombre: 'LINDLEY SAC',
+    cuenta: '',
+  };
+  gasto: Gasto = {
+    rubro: '',
+    nota: '',
+  }
+  rubros: Option[] = [
+    {value: 'HONORARIOS', viewValue: 'Honorarios'},
+    {value: 'ALQUILERES', viewValue: 'Alquileres'},
+    {value: 'SUMINISTROS_DIVERSOS', viewValue: 'Suministros Diversos'},
+    {value: 'COMUNICACIONES', viewValue: 'Comunicaciones'},
+    {value: 'REPARACION_MANTENIMIENTO', viewValue: 'Reparacion y Mantenimiento'},
+    {value: 'OTROS_SERVICIOS', viewValue: 'Otros Servicios'},
+  ];
+
+  tipoDocs: Option[] = [
+    {value: 'DNI', viewValue: 'DNI'},
+    {value: 'RUC', viewValue: 'RUC'},
+  ];
+
+  cuentas: Option[] = [
+    {value: '10000053', viewValue: '10000053'},
+    {value: '10000189', viewValue: '10000189'},
+  ];
+
+  autorizadores: Option[] = [
+    {value: 'GRIOS', viewValue: 'grios'},
+    {value: 'EPEREZ', viewValue: 'eperez'},
+    {value: 'MFERNANDEZ', viewValue: 'mfernandez'},
+    {value: 'JCABELLOC', viewValue: 'jcabelloc'},
+  ];
+
+  
+  tiposCuentaEgreso: Option[] = [
+    {value: 'CAJA', viewValue: 'CAJA'},
+    {value: 'BANCOS', viewValue: 'BANCOS'},
+  ];
+
+  cuentasEgreso: Option[] = [
+    {value: '10000053', viewValue: '10000053'},
+    {value: '10000189', viewValue: '10000189'},
+  ];
+  isRecaudoBanco: boolean = true;
 
   constructor() { }
 
--- src/app/operaciones/administrativas/registro-gasto/registro-gasto.module.ts
+++ src/app/operaciones/administrativas/registro-gasto/registro-gasto.module.ts
@@ -1,15 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule }   from '@angular/forms';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
 import { RegistroGastoRoutingModule } from './registro-gasto-routing.module';
 import { AdmRegistroGastoComponent } from './components/adm-registro-gasto/adm-registro-gasto.component';
 import { CreateRegistroGastoComponent } from './components/create-registro-gasto/create-registro-gasto.component';
 import { ShowRegistroGastoComponent } from './components/show-registro-gasto/show-registro-gasto.component';
 
+
 @NgModule({
   imports: [
     CommonModule,
-    RegistroGastoRoutingModule
+    RegistroGastoRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatCardModule,
+    FormsModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatTableModule,
+    MatExpansionModule,
   ],
   declarations: [AdmRegistroGastoComponent, CreateRegistroGastoComponent, ShowRegistroGastoComponent]
 })
