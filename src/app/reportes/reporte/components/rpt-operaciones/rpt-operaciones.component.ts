843519471aee9aeeb1b30cf7ae68a0e049c7544f
Juan Cabello <jcabelloc@gmail.com>
2019-02-06 10:12:48 -0600

    Add filter to Reporte de Operaciones

--- src/app/reportes/reporte/components/rpt-operaciones/rpt-operaciones.component.ts
+++ src/app/reportes/reporte/components/rpt-operaciones/rpt-operaciones.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
 
+interface Option {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-rpt-operaciones',
   templateUrl: './rpt-operaciones.component.html',
@@ -10,6 +15,16 @@ export class RptOperacionesComponent implements OnInit {
   readonly reporteUrl: string = environment.reporteUrl;
   desde: string;
   hasta: string;
+  tipoOperacion: string = '';
+  tiposOperacion: Option[] = [
+    {value: 'DESEMBOLSO_CRED', viewValue: 'Desembolsos'}, 
+    {value: 'AMORTIZACION_CRED', viewValue: 'Amortizaciones'},
+    {value: 'GASTO_ADM', viewValue: 'Gastos Adm.'},
+    {value: 'HABILITACION_CAJA', viewValue: 'Habilitacion de Caja'},
+    {value: 'REMESA_BANCO', viewValue: 'Remesa a Banco '},
+    {value: 'TODOS', viewValue: 'Todos'},
+  ];
+  // DESEMBOLSO_CRED, AMORTIZACION_CRED, GASTO_ADM, HABILITACION_CAJA, REMESA_BANCO, APORTE_CAPITAL
 
   constructor() { }
 
@@ -25,8 +40,9 @@ export class RptOperacionesComponent implements OnInit {
   }
   onSubmit({value, valid}: {value: any, valid: boolean}) {
     if (valid) {
-      let rptUrl = this.reporteUrl + "operaciones?desde=" + this.desde 
-      + "&hasta=" + this.hasta;
+      this.tipoOperacion = this.tipoOperacion == 'TODOS'? '':this.tipoOperacion;
+      let rptUrl = this.reporteUrl + "operaciones?tipoOperacion=" + this.tipoOperacion + 
+      "&desde=" + this.desde  + "&hasta=" + this.hasta;
       console.log(rptUrl);
       window.open(rptUrl);
     }
